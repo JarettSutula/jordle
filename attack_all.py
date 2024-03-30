ee3dfca4905be0d131fc0427ccc48ccd952db535
JarettSutula <JarettSutula1@marist.edu>
2024-03-30 00:27:19 -0400

    averaging just over 4 guesses, no failures. bar graph in.

--- attack_all.py
+++ attack_all.py
@@ -1,5 +1,6 @@
 from jordle import Jordle
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 prev_answers = []
 with open('previous_wordle_answers.txt', 'r') as file:
@@ -8,9 +9,11 @@ with open('previous_wordle_answers.txt', 'r') as file:
 
 scores = {'1': 0, '2': 0, '3': 0, '4':0, '5':0, '6':0, 'X':0}
 failures = []
+avg = 0
+starting_word = "arise"
 for i in tqdm(range(len(prev_answers))):
     answer = prev_answers[i]
-    test_jordle = Jordle("arise", answer)
+    test_jordle = Jordle(starting_word, answer)
     test_jordle.populate_banks()
     while len(test_jordle.final_guesses) < 6:
         # get results from the last guess before checking / trying again.
@@ -19,6 +22,7 @@ for i in tqdm(range(len(prev_answers))):
         if test_jordle.answer == test_jordle.guess:
             # print(f"Jordle found the answer after {len(test_jordle.final_guesses)} guesses.")
             scores[str(len(test_jordle.final_guesses))] += 1
+            avg += len(test_jordle.final_guesses)
             break
 
         else:
@@ -30,7 +34,18 @@ for i in tqdm(range(len(prev_answers))):
     if test_jordle.final_guesses[-1] != test_jordle.answer:
         failures.append(test_jordle.answer)
         scores['X'] += 1
+        avg += 7 
         # print(f"Jordle did not guess the answer correctly after {len(test_jordle.final_guesses)} guesses.")
 
     # test_jordle.output()
+avg /= len(prev_answers)
+avg = round(avg, 3)
 print(scores)
+print(avg)
+score = list(scores.keys())
+values = list(scores.values())
+plt.bar(range(len(score)), values, tick_label=score)
+plt.ylabel('Number of words')
+plt.title("""Performance of Jordle starting with word '""" + starting_word +"""'""")
+plt.xlabel('Number of Guesses')
+plt.show()
